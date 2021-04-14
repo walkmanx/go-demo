6574f91accb98fe11774c47f74b6f41304eedd80
walkmanx <guochen@zhuoyuan-info.com>
2021-04-14 14:30:59 +0800

    add code

--- src/example10/channel.go
+++ src/example10/channel.go
@@ -6,6 +6,12 @@ import (
 
 func main() {
 
+	rangeFun()
+}
+
+// 通过for循环遍历channel的内容
+func forechFun() {
+
 	//创建无缓冲channel
 	ch := make(chan int)
 
@@ -25,5 +31,24 @@ func main() {
 			fmt.Println("管道关闭")
 			break
 		}
+	}
+
+}
+
+// 通过range遍历channel内容
+func rangeFun() {
+	//创建无缓冲channel
+	ch := make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			fmt.Println("子线程 i=", i)
+			ch <- i
+		}
+		close(ch)
+	}()
+
+	for num := range ch {
+		fmt.Println("num=", num)
 	}
 }
